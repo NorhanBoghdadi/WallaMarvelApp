91f0fa53d00ca3b1d0334d19221252e07e325ec8
norhan-boghdadi <n.boghdadi@foodics.com>
2025-02-06 14:55:13 +0200

    add marvel endpoints

--- WallaMarvel/Data/DataModel/APIEndpoint.swift
+++ WallaMarvel/Data/DataModel/APIEndpoint.swift
@@ -10,5 +10,26 @@ import Foundation
 // MARK: - Endpoint Protocol
 protocol APIEndpoint {
     var path: String { get }
-    var queryItems: [URLQueryItem]? { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+enum MarvelEndpoint: APIEndpoint {
+    case characters
+
+    var path: String {
+        switch self {
+        case .characters:
+            return "/v1/public/characters"
+        }
+    }
+
+    var queryItems: [URLQueryItem] {
+        var items = [URLQueryItem]()
+
+        switch self {
+        default:
+            break
+        }
+        return items
+    }
 }
