0223954bf87f6641c6cfa574da8983185b0f28b5
norhan-boghdadi <n.boghdadi@foodics.com>
2025-02-08 11:29:27 +0200

    change tableview cell

--- WallaMarvel/List Heroes/ListHeroesTableView.swift
+++ WallaMarvel/List Heroes/ListHeroesTableView.swift
@@ -2,43 +2,40 @@ import Foundation
 import UIKit
 
 final class ListHeroesView: UIView {
-    enum Constant {
-        static let estimatedRowHeight: CGFloat = 120
+    enum Layout {
+        static let padding: CGFloat = 16
+        static let spacing: CGFloat = 12
     }
-    
+
     let heroesTableView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView(frame: .zero, style: .plain)
         tableView.register(ListHeroesTableViewCell.self, forCellReuseIdentifier: "ListHeroesTableViewCell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = Constant.estimatedRowHeight
+        tableView.backgroundColor = .black
+        tableView.separatorStyle = .none
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: Layout.padding, left: 0, bottom: Layout.padding, right: 0)
         return tableView
     }()
-    
+
     init() {
         super.init(frame: .zero)
-        setup()
+        setupViews()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func setup() {
-        addSubviews()
-        addContraints()
-    }
-    
-    private func addSubviews() {
+
+    private func setupViews() {
+        backgroundColor = .black
         addSubview(heroesTableView)
-    }
-    
-    private func addContraints() {
+        heroesTableView.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
+            heroesTableView.topAnchor.constraint(equalTo: topAnchor),
             heroesTableView.leadingAnchor.constraint(equalTo: leadingAnchor),
             heroesTableView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            heroesTableView.topAnchor.constraint(equalTo: topAnchor),
-            heroesTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            heroesTableView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
 }
--- WallaMarvel/List Heroes/ListHeroesTableViewCell.swift
+++ WallaMarvel/List Heroes/ListHeroesTableViewCell.swift
@@ -3,53 +3,100 @@ import UIKit
 import Kingfisher
 
 final class ListHeroesTableViewCell: UITableViewCell {
-    private let heroeImageView: UIImageView = {
+    private enum Layout {
+        static let padding: CGFloat = 16
+        static let imageHeight: CGFloat = 170
+        static let cornerRadius: CGFloat = 12
+    }
+
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.1, alpha: 1.0)
+        view.layer.cornerRadius = Layout.cornerRadius
+        view.clipsToBounds = true
+        return view
+    }()
+
+    private let heroImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
         return imageView
     }()
-    
-    private let heroeName: UILabel = {
+
+    private let overlayView: UIView = {
+        let view = UIView()
+        let gradient = CAGradientLayer()
+        gradient.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.8).cgColor
+        ]
+        gradient.locations = [0.4, 1.0]
+        view.layer.addSublayer(gradient)
+        return view
+    }()
+
+    private let nameLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .white
         label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setup()
+        setupViews()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func setup() {
-        addSubviews()
-        addContraints()
-    }
-    
-    private func addSubviews() {
-        addSubview(heroeImageView)
-        addSubview(heroeName)
-    }
-    
-    private func addContraints() {
+
+    private func setupViews() {
+        backgroundColor = .clear
+        selectionStyle = .none
+
+        [containerView, heroImageView, overlayView, nameLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        contentView.addSubview(containerView)
+        containerView.addSubview(heroImageView)
+        containerView.addSubview(overlayView)
+        containerView.addSubview(nameLabel)
+
         NSLayoutConstraint.activate([
-            heroeImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
-            heroeImageView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
-            heroeImageView.heightAnchor.constraint(equalToConstant: 80),
-            heroeImageView.widthAnchor.constraint(equalToConstant: 80),
-            heroeImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
-            
-            heroeName.leadingAnchor.constraint(equalTo: heroeImageView.trailingAnchor, constant: 12),
-            heroeName.topAnchor.constraint(equalTo: heroeImageView.topAnchor, constant: 8),
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Layout.padding/2),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Layout.padding),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Layout.padding),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Layout.padding/2),
+
+            heroImageView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            heroImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            heroImageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            heroImageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+
+            overlayView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            overlayView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            overlayView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            overlayView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+
+            nameLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: Layout.padding),
+            nameLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -Layout.padding),
+            nameLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -Layout.padding)
         ])
     }
-    
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        overlayView.layer.sublayers?.first?.frame = overlayView.bounds
+    }
+
     func configure(model: CharacterDataModel) {
-        heroeImageView.kf.setImage(with: URL(string: model.thumbnail.path + "/portrait_small." + model.thumbnail.extension))
-        heroeName.text = model.name
+        nameLabel.text = model.name
+        if let url = URL(string: model.thumbnail.path + "/landscape_incredible." + model.thumbnail.extension) {
+            heroImageView.kf.setImage(with: url)
+        }
     }
 }
